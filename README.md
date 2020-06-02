dad12583f85bcd1c9b2e7630697c8d657a1eb905
YtMa <ytma98@gmail.com>
2020-06-02 16:27:04 -0700

    Add volume level to notification

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Yin
+# Yin 音
  My personal audio utility for Android
+
+## Functionality
+
+1. Notify when headset is plugged or unplugged.
--- app/src/main/java/me/ytma/yin/HeadsetStateBroadcastReceiver.kt
+++ app/src/main/java/me/ytma/yin/HeadsetStateBroadcastReceiver.kt
@@ -3,6 +3,7 @@ package me.ytma.yin
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
@@ -12,19 +13,29 @@ class HeadsetStateBroadcastReceiver : BroadcastReceiver() {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
         if (Intent.ACTION_HEADSET_PLUG == intent.action) {
             val state = intent.getIntExtra("state", 0)
-            val name = intent.getStringExtra("name")
-            val hasMicrophone = intent.getBooleanExtra("microphone", false)
+            val hasMicrophone = intent.getIntExtra("microphone", 0)
             val builder = NotificationCompat.Builder(context, "HeadsetStateChannel")
+            val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+            val volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
             if (state > 0) {
                 // Plugged
-                builder.setSmallIcon(R.drawable.ic_headset_plug)
-                    .setContentTitle("Headset plugged")
-            } else {
+                if (hasMicrophone == 1) {
+                    builder.setSmallIcon(R.drawable.ic_headset_with_microphone_plug)
+                        .setContentTitle("Headset with microphone plugged")
+                        .setContentText("Current volume level: $volume")
+                } else {
+                    builder.setSmallIcon(R.drawable.ic_headset_plug)
+                        .setContentTitle("Headset plugged")
+                        .setContentText("Current volume level: $volume")
+                }
+                NotificationManagerCompat.from(context).notify(0, builder.build())
+            } else if (state == 0) {
                 // Unplugged
                 builder.setSmallIcon(R.drawable.ic_headset_unplug)
                     .setContentTitle("Headset unplugged")
+                    .setContentText("Current volume level: $volume")
+                NotificationManagerCompat.from(context).notify(0, builder.build())
             }
-            NotificationManagerCompat.from(context).notify(0, builder.build())
         }
     }
 }
